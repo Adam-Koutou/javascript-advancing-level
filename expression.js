e8325c4e0b39b6c085fbab7bc00082acfa880b25
Adam Koutou <jeroldkoutou@gmail.com>
2025-04-30 23:02:32 +0200

    Add files via upload
    Introduction to javascript expression

---
+++ expression.js
@@ -0,0 +1,23 @@
+// Use expression to perform some calculs
+let x = 0;
+x = x + 1;
+
+// you've a lot of options to use expression
+// we've incrementation of variable x
+++x; 
+x++;
+x += 1;
+x = (-(~x));
+
+
+// you can test youre own expression for training more
+// like this one
+let number1 = 1
+let number2 = 3
+let addition = number1 + number2
+addition        // get the value of youre expression
+
+// second example
+const i = 2
+let complexe = ((number1*2)/number2**i - (number2*i))/i**3
+complexe        
